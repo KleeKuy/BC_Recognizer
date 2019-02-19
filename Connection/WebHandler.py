c7514c407b2e58dda3bddbac22ab0d2b5cc135d1
KleeKuy <klikuy@gmail.com>
2019-02-19 12:26:38 +0100

    Completely redone WebHandler class. New WebHandler uses python select and is fully synchronous. General idea did not change that much however, Webhandler is still using labmdas given at creation.

--- Connection/WebHandler.py
+++ Connection/WebHandler.py
@@ -1,6 +1,4 @@
-import socket
-from time import sleep
-import logging
+import select, socket, queue
 BUFFER_SIZE = 1024
 
 
@@ -8,77 +6,82 @@ class WebHandler:
 
     def __init__(self,
                  handlers):
-        self._handlers = handlers
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setblocking(0)
+        self.server.bind(('', 50000))
+        self.server.listen(15)
+        self.inputs = [self.server]
+        self.outputs = []
+        self.message_queues = {}
+        self.handlers = handlers
 
-    def setup_connection(self,
-                         port):
-        setup = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        setup.bind(('', port))
-        setup.setblocking(0)
-        setup.listen(1)
-        print("listening")
-        while True:
-            try:
-                c, a = setup.accept()
-            except BlockingIOError:
-                sleep(1)
-                continue
-            break
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind(('', 0))
-        port = sock.getsockname()[1]
-        print(port)
-        c.send(port.to_bytes((port.bit_length() + 7) // 8, byteorder='big'))
-        setup.close()
-        sock.setblocking(0)
-        sock.listen(1)
-        while True:
-            try:
-                conn, addr = sock.accept()
-            except BlockingIOError:
-                sleep(1)
-                continue
-            break
-        logging.info('Connection address:', addr)  # TODO add logging? logging to file?
-        return sock, conn
+    def run(self):
+        print('run start')
+        while self.inputs:
+            readable, writable, exceptional = select.select(
+                self.inputs, self.outputs, self.inputs)
+            for s in readable:
+                self.read(s)
+            for s in writable:
+                self.write(s)
+            for s in exceptional:
+                self.clear(s)
 
-    def receive_command(self,
-                        conn):
-        while True:
-            try:
-                cmd = conn.recv(3)
-            except BlockingIOError:
-                sleep(1)
-                continue
-            break
-        cmddec = cmd.decode('utf-8')
-        logging.info('Command received :', cmddec)
-        return cmddec
+    def write(self,
+              s):
+        try:
+            next_msg = self.message_queues[s].get_nowait()
+        except queue.Empty:
+            self.outputs.remove(s)
+        except KeyError:
+            print("socket already closed!")
+        else:
+            s.send(next_msg.encode('utf-8'))
 
-    def receive_data(self,
-                     conn):
-        data = conn.recv(BUFFER_SIZE)
+    def read(self,
+             s):
+        if s is self.server:
+            self.accept(s)
+        else:
+            cmd = s.recv(3)
+            if cmd:
+                cmddec = cmd.decode('utf-8')
+                if cmddec == 'END':
+                    self.clear(s)
+                    return
+                print(cmddec)
+                data = self.receive(s)
+                self.message_queues[s].put(self.handlers[cmddec](data))
+                if s not in self.outputs:
+                    self.outputs.append(s)
+            else:
+                self.clear(s)
+
+    def accept(self,
+               s):
+        connection, client_address = s.accept()
+        connection.setblocking(0)
+        self.inputs.append(connection)
+        self.message_queues[connection] = queue.Queue()
+
+    def clear(self,
+              s):
+        self.inputs.remove(s)
+        if s in self.outputs:
+            self.outputs.remove(s)
+        s.close()
+        del self.message_queues[s]
+
+    def receive(self,
+                s):
+        data = s.recv(BUFFER_SIZE)
         while True:
             try:
-                rec = conn.recv(BUFFER_SIZE)  # are we 100% certain that this works in every condition?
+                rec = s.recv(BUFFER_SIZE)  # are we 100% certain that this works in every condition?
+                if rec is None:
+                    print(rec)
+                    break
             except BlockingIOError:
                 break
             data += rec
-        return data.decode('utf-8')
-
-    def listen(self,
-               conn,
-               sock):
-        while True:
-            cmd = self.receive_command(conn)
-            if cmd == "END":
-                break
-            data = self.receive_data(conn)
-            try:
-                response = self._handlers[cmd](data).encode('utf-8')
-            except KeyError:
-                conn.send("No such command")
-                continue
-            conn.send(response)
-        conn.close()
-        sock.close()
+        return data
--- Connection/WebHandler_unittest.py
+++ Connection/WebHandler_unittest.py
@@ -1,33 +1,29 @@
 import unittest
 from threading import Thread
-from Connection.WebHandler import WebHandler
 import socket
 import base64
-from time import sleep
+from Connection.WebHandler import WebHandler
 BUFFER_SIZE = 1024
 
 
 class WebServetTest(unittest.TestCase):
     def setUp(self):
-
         self.TCP_IP = '127.0.0.1'
-        self.TCP_PORT = 5005
+        self.TCP_PORT = 50000
         self.handlers = {
             'IMG': lambda data: self.img_handle(data),
             'STR': lambda data: self.str_handle(data)
         }
         self.web = WebHandler(handlers=self.handlers)
         self.res = None
-        t = Thread(target=self.server_simulator, daemon=True)
+        t = Thread(target=self.web.run, daemon=True)
         t.start()
 
     def send(self,
              data,
              cmd):
-        port = self.get_port()
-        print("rec port " + str(port))
+        port = self.TCP_PORT
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sleep(1)
         s.connect((self.TCP_IP, port))
         s.send(cmd)
         s.send(data)
@@ -36,14 +32,6 @@ class WebServetTest(unittest.TestCase):
         s.send('END'.encode('utf-8'))
         s.close()
         self.res = msg
-
-    def get_port(self):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.TCP_IP, self.TCP_PORT))
-        rec = s.recv(BUFFER_SIZE)
-        s.close()
-        port = int.from_bytes(rec, byteorder='big')
-        return port
 
     def test_img(self):
         cmdenc = 'IMG'.encode('utf-8')
@@ -54,32 +42,20 @@ class WebServetTest(unittest.TestCase):
         thread.join()
         self.assertEqual(self.res, "text retrieved from image", "Image send test failed")
 
-    def test_str(self):
+    def test_str(self,
+                 data="Test message!"):
         cmdenc = 'STR'.encode('utf-8')
-        data = "Test message!"
-        thread = Thread(target=self.send, args=(data.encode('utf-8'), cmdenc))
-        thread.start()
-        thread.join()
+        self.send(data.encode('utf-8'), cmdenc)
         self.assertEqual(self.res, data, "Image send test failed")
 
     def test_many_connections(self):
         i = 0
-       # while i < 5:
-        t1 = Thread(target=self.test_img)
-        t2 = Thread(target=self.test_img)
-        t1.start()
-        sleep(1)
-        print("test 2 ")
-        t2.start()
-        #sleep(0.2)
-        i += 1
-
-    def server_simulator(self):
-        while True:
-            print("server thread!")
-            sock, conn = self.web.setup_connection(self.TCP_PORT)
-            thread = Thread(target=self.web.listen, args=(conn, sock))
-            thread.start()
+        while i < 10:
+            t1 = Thread(target=self.test_str, args=str(i))
+            t2 = Thread(target=self.test_img)
+            t1.start()
+            t2.start()
+            i += 1
 
     def img_handle(self,
                    data):
@@ -90,10 +66,10 @@ class WebServetTest(unittest.TestCase):
 
     def str_handle(self,
                    data):
-        return data
+        return data.decode('utf-8')
 
     def run_all(self):
         self.setUp()
         self.test_img()
-        self.test_str()
-        #self.test_many_connections()
+        self.test_str() #TODO sometimes it can happen that we have delay after command and data is new command
+        self.test_many_connections()
